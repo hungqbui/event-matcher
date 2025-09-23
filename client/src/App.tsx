e7bfdf502621ecd6f0e7321212e98577d4684fd9
brix-diana <brixdiana312@gmail.com>
2025-09-23 15:28:50 -0500

    Added basic profile page, change later

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './Homepage';
 import Login from './Auth/Login';
 import Signup from './Auth/Signup';
+import ProfileForm from './profile/profile';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-
+      <Route path="/profile" element={<ProfileForm user={{name: '', email: ''}} onSave={() => {}} />} />
     </Routes>
   );
 }
